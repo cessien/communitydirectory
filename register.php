1a22c11f650de32c4828a014cfdabdbf5c316bcc
Charles <cessien88@gmail.com>
2014-05-18 20:31:56 -0400

    started building out a navrbar

--- register.php
+++ register.php
@@ -20,6 +20,27 @@ $_SESSION["current_step"] = "person"; //The current step of the wizard. 1 - pers
         <link type="text/css" media="all" rel="stylesheet" href="<?php echo $dir;?>css/npccommunity.css">
     </head>
     <body ng-controller="main-controller">
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">NPC Community Directory</a>
+            </div>
+            <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
         <section class="jumbotron text-center">
             <h1>Signing up: <span style="text-transform: uppercase;"><?php if($current_user->user_login !="") echo $current_user->user_login?></span><br ><small>Let's start with some basic info about yourself.</small></h1>
         </section>
@@ -27,7 +48,7 @@ $_SESSION["current_step"] = "person"; //The current step of the wizard. 1 - pers
             <div id="main-view" ng-controller="view-controller" ></div>
             <div class="row navigation">
                 <div class="col-sm-6">
-                    <h1 class="previous"><a ng-href="#" ng-click="paginate('family')">&lt; Previous</a></h1>
+                    <h1 class="previous"><a ng-href="#" ng-click="paginate('prev')">&lt; Previous</a></h1>
                 </div>
                 <div class="col-sm-6">
                     <h1 class="next" ><a ng-href="#" ng-click="submitAll('next')">Continue &gt;</a></h1>
