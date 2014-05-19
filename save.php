1b4c3ec958a48c8201c143d5dca6a797dfede499
Simon Ma <raistlinreborn@gmail.com>
2014-05-18 20:44:10 -0400

    Added family functions

--- save.php
+++ save.php
@@ -31,7 +31,7 @@
 		$_SESSION['person']['address_line1'] = $_POST['address_line1'];
 		$_SESSION['person']['address_line2'] = $_POST['address_line2'];
 		$_SESSION['person']['city'] = $_POST['city'];
-		$_SESSION['person']['zip'] = $_POST['zip'];
+		$_SESSION['person']['zipcode'] = $_POST['zipcode'];
 		$_SESSION['person']['state'] = $_POST['state'];
         echo "successfully uploaded location information";
     }  else if ($action == "contact-info"){
@@ -48,25 +48,51 @@
 			$_SESSION['person']['middle_name'] = $_POST['middle_name'];
 			$_SESSION['person']['last_name'] = $_POST['last_name'];
 
-//			$_SESSION['person']['profile_picture'] = $_POST['profile_picture'];
+//            $_SESSION['person']['birthday'] = $_POST['birthday'];
+//            $_SESSION['person']['sex'] = $_POST['sex'];
 //
-//			$_SESSION['person']['birthday'] = $_POST['birthday'];
-//			$_SESSION['person']['sex'] = $_POST['sex'];
+//            $_SESSION['person']['profile_picture'] = $_POST['profile_picture'];
 //
-//			$_SESSION['person']['address_line1'] = $_POST['address_line1'];
-//			$_SESSION['person']['address_line2'] = $_POST['address_line2'];
-//			$_SESSION['person']['city'] = $_POST['city'];
-//			$_SESSION['person']['zip'] = $_POST['zip'];
-//			$_SESSION['person']['state'] = $_POST['state'];
+//            $_SESSION['person']['address_line1'] = $_POST['address_line1'];
+//            $_SESSION['person']['address_line2'] = $_POST['address_line2'];
+//            $_SESSION['person']['city'] = $_POST['city'];
+//            $_SESSION['person']['zipcode'] = $_POST['zipcode'];
+//            $_SESSION['person']['state'] = $_POST['state'];
 //
-//			$_SESSION['person']['primary_email'] = $_POST['primary_email'];
-//			$_SESSION['person']['primary_phone'] = $_POST['primary_phone'];
+//            $_SESSION['person']['primary_email'] = $_POST['primary_email'];
+			$_SESSION['person']['primary_phone'] = $_POST['primary_phone'];
+            $_SESSION['person']['member'] = $_POST['member'];
 
             $_SESSION['person']['date_updated'] = current_time('mysql', '1');
 
-            echo "lalala";
+            echo "Create new person";
 			createPerson($_SESSION['person']);
-		}
+		} else if($type == "family"){
+            $fam_action = $_POST['action'];
+            if ($fam_action == "create" || $fam_action == "update"){
+                $_SESSION['family']['name'] = $_POST['name'];
+                $_SESSION['family']['primary_email'] = $_POST['primary_email'];
+                $_SESSION['family']['secondary_email'] = $_POST['secondary_email'];
+                $_SESSION['family']['primary_email'] = $_POST['primary_email'];
+                $_SESSION['family']['secondary_email'] = $_POST['secondary_email'];
+                $_SESSION['family']['address_line1'] = $_POST['address_line1'];
+                $_SESSION['family']['address_line2'] = $_POST['address_line2'];
+                $_SESSION['family']['city'] = $_POST['city'];
+                $_SESSION['family']['zipcode'] = $_POST['zipcode'];
+
+                $_SESSION['person']['date_updated'] = current_time('mysql', '1');
+
+                if($fam_action == "create"){
+                    $_SESSION['person']['date_created'] = current_time('mysql', '1');
+                    echo "Create new family";
+                    createFamily($_SESSION['family']);
+                } else {
+                    echo "Updating family";
+
+                }
+            }
+
+        }
 	}
 
 
@@ -80,6 +106,7 @@
 		);
         if ($result === 1){
             echo "uid:".$wpdb->insert_id;
+            $_SESSION['person']['uid'] = $wpdb->insert_id;
         } else {
             echo "failed to insert new person";
         }
@@ -98,7 +125,7 @@
         if ($result === 1){
             echo "udpated person".$wpdb->insert_id;
         } else {
-            echo "failed to update new person";
+            echo "failed to update person";
         }
     }
 
@@ -120,10 +147,35 @@
     function createFamily($family){
         global $wpdb;
         global $TABLE_FAMILY;
-        $wpdb->insert(
+        $result = $wpdb->insert(
             $TABLE_FAMILY,
             $family
         );
+        if ($result === 1){
+            echo "uid:".$wpdb->insert_id;
+            $_SESSION['family']['uid'] = $wpdb->insert_id;
+            $_SESSION['person']['family_uid'] = $wpdb->insert_id;
+            editPerson($_SESSION['person']);
+        } else {
+            echo "failed to insert new person";
+        }
+    }
+
+    function editFamily($family){
+        global $wpdb;
+        global $TABLE_FAMILY;
+        $result = $wpdb->update(
+            $TABLE_FAMILY,
+            $family,
+            array( 'uid' => $family->uid ),
+            null,
+            array( '%d' )
+        );
+        if ($result === 1){
+            echo "udpated family".$wpdb->insert_id;
+        } else {
+            echo "failed to update family";
+        }
     }
 
     //TODO get final structure of family and members
