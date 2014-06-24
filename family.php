da470a08d72d21d81ffe1a614f84ee980ff8b6d4
Simon Ma <raistlinreborn@gmail.com>
2014-06-23 23:48:10 -0400

    Families UI implemented. CSS requires standardization between all views.

--- family.php
+++ family.php
@@ -5,17 +5,17 @@ if(!session_id()) {
 
 $dir = "/wordpress/wp-content/plugins/npcdirectory/";
 $upload_dir = "/wordpress/wp-content/uploads/profile/";
-global $current_user;
+global $family_user;
 $user_info = get_currentuserinfo();
 ?>
 <?php include("header.php"); ?>
             <div class="row">
                 <div class="col-md-1"></div>
                 <div class="col-md-10 col-sm-12">
-                    <article ng-controller="people-view">
+                    <article ng-controller="family-view">
                         <div class="form-group">
                             <div class="row section search">
-                                <h2 class="col-sm-12">People in NPC</h2>
+                                <h2 class="col-sm-12">Families in NPC</h2>
                                 <div class="input-group input-group-lg col-sm-12">
                                     <input type="text" class="search-query form-control" ng-model="keywords" ng-click="family.active = false" ng-keyup="search()" placeholder="Search for families by last name">
                                     <span class="input-group-btn">
@@ -24,20 +24,33 @@ $user_info = get_currentuserinfo();
                                         </button>
                                     </span>
                                 </div>
+                            </div>
+                            <div class="row text-left actions">
+                                <div class="col-md-2"><a ng-click="selectAll()" href><h2><span class="glyphicon glyphicon-ok"></span> Select all</h2></a></div>
+                                <div class="col-md-3"><a ng-click="email()" href><h2><span class="glyphicon glyphicon-envelope"></span> Email selected</h2></a></div>
+                                <div class="col-md-4"><a ng-click="listNumbers()" href><h2><span class="glyphicon glyphicon-earphone"></span> Selected phone numbers</h2></a></div>
+                            </div>
+                            <div class="row text-center">
+                                <div class="col-md-5 text-left actions">
+                                    <h2>Viewing ({{mode}}):</h2>
+                                    <a ng-click="mode='images'"><h2><span class="glyphicon glyphicon-th"></span> Images</h2></a>
+                                    <a ng-click="mode='list'"><h2><span class="glyphicon glyphicon-th-list"></span> List</h2></a>
+                                </div>
+                            </div>
+                            <div class="row section search">
                                 <div class="col-sm-12">
-                                    <div id="people" class="results row">
+                                    <div id="families" class="results row">
                                         <div class="col-md-4" ng-repeat="family in list | filter: filterfn">
-                                            <div class="people-header" ng-repeat="person in family" ng-click="person.selected = (person.selected)?false:true">
-                                                    <img ng-src="<?php echo $upload_dir;?>{{person.profile_picture}}" class="img-responsive" alt="Responsive image"/>
+                                            <div class="family-header" ng-click="family.selected = (family.selected)?false:true">
+                                                <img src="<?php echo $dir;?>img/default-profile.png" class="img-responsive" alt="Responsive image"/>
                                                 <div class="information">
-                                                    
-                                                    <h2 ng-click="showModal(person.uid)">
-                                                        <div class="selected" ng-show="person.selected" ng-click="person.selected = false"><h2><span class="glyphicon glyphicon-ok"></h2></div>
-                                                        <strong>{{person.first_name}}</strong> {{person.last_name}}</h2>
-                                                    <div class="details">
-                                                        <h4>{{person.primary_email}} <a ng-href="mailto:{{person.primary_email}}"><span class="glyphicon glyphicon-envelope"></span></a></h4>
-                                                        <h4 class="phone">{{person.primary_phone}} <a ng-click="showModal(person.uid,'number')"><span class="glyphicon glyphicon-earphone"></span></a></h4>
-                                                        <h4>{{person.address_line1}} <a ng-href=><span class="glyphicon glyphicon-home"></span></a></h4>
+                                                    <h2 ng-click="showModal(family.uid)">
+                                                        <div class="selected" ng-show="family.selected" ng-click="family.selected = false"><h2><span class="glyphicon glyphicon-ok"></h2></div>
+                                                        <strong>{{family.name}}</strong>
+                                                    </h2>
+                                                    <div>
+                                                        <h3><strong>{{family.city}},&nbsp;{{family.state}}</strong></h3>
+                                                        <h3><span ng-repeat="person in family.members">{{person}}{{$last ? '' : ', '}}</span></h3>
                                                     </div>
                                                 </div>
                                             </div>
@@ -46,7 +59,9 @@ $user_info = get_currentuserinfo();
                                 </div>
                             </div>
                         </div>
-                        <div id="people-modal" class="modal fade">
+
+                        <!-- Family details modal -->
+                        <div id="family-modal" class="modal fade">
                             <div class="modal-dialog">
                                 <article class="modal-content">
                                     <div class="modal-header">
@@ -54,7 +69,6 @@ $user_info = get_currentuserinfo();
                                         <img ng-src="<?php echo $upload_dir;?>{{selection.profile_picture}}" width=70 alt="Responsive image"/>
                                         <div>
                                             <h2 class="modal-title">{{selection.header}}</h2>
-                                            <h3>{{selection.header2}}</h3>
                                         </div>
                                     </div>
                                     <div class="modal-body information">
@@ -70,15 +84,67 @@ $user_info = get_currentuserinfo();
                                                     <p class="secondary">{{selection.city}} {{selection.state}}, {{selection.zipcode}}</p>
                                                 </td>
                                             </tr>
-                                            <tr><td><p>Age:</p></td>
+                                            <tr><td><p>Members:</p></td>
                                                 <td>
-                                                    <p><strong>{{selection.age}}</strong><span class="secondary"> ({{selection.birthday}})</span></p>
+                                                    <p ng-repeat="person in selection.members"><strong>{{person}}</strong></p>
                                                 </td>
                                             </tr>
                                         </table>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </article><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
+                        </div><!-- /.modal -->
+
+                        <!-- emails modal-->
+                        <div id="email-modal" class="modal fade">
+                            <div class="modal-dialog">
+                                <article class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <div>
+                                            <h2 class="modal-title">Email addresses</h2>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body information">
+                                        <form class="form-group">
+                                            <h2>Copy and paste this text field into an email</h2>
+                                            <div class="input-group input-group-lg">
+                                                <textarea ng-model="emailList" style="width: 500px; height 200px;"></textarea>
+                                            </div>
+                                        </form>
+                                        <table class="table" >
+                                            <tr><th>Family</th><th>Email addresses</th></tr>
+                                            <tr ng-repeat="family in list" ng-show="family.selected == true && family.primary_email != ''"><td><p>{{family.name}}</p></td><td><p><strong>{{family.primary_email}}</strong></p><p ng-show="family.secondary_email != ''" class="secondary">{{family.secondary_email}}</p></td><td><a ng-href="mailto:{{family.primary_email}}"><span class="glyphicon glyphicon-envelope"></span></a></td></tr>
+                                        </table>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-lg btn-primary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </article><!-- /.modal-content -->
+                            </div><!-- /.modal-dialog -->
+                        </div><!-- /.modal -->
+
+                        <!-- phone numbers modal-->
+                        <div id="numbers-modal" class="modal fade">
+                            <div class="modal-dialog">
+                                <article class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <div>
+                                            <h2 class="modal-title">Phone numbers</h2>
+                                        </div>
+                                    </div>
+                                    <div class="modal-body information">
+                                        <table class="table" >
+                                            <tr><th>Family</th><th>Phone numbers</th></tr>
+                                            <tr ng-repeat="family in list" ng-show="family.selected == true && family.primary_phone != ''"><td><p>{{family.name}}</p></td><td><p><strong>{{family.primary_phone}}</strong></p><p ng-show="family.secondary_phone != ''" class="secondary">{{family.secondary_phone}}</p></td></tr>
+                                        </table>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-lg btn-primary" data-dismiss="modal">Close</button>
                                     </div>
                                 </article><!-- /.modal-content -->
                             </div><!-- /.modal-dialog -->
